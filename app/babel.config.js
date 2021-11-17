9c841297e097634c3d38f0d74cc67ba59914af0a
Pierre COLLET <pierre61250@yahoo.fr>
2021-11-17 14:34:34 +0100

    Correction Structure Projet React

--- app/babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    presets: ['@babel/preset-env', '@babel/preset-react'],
-    plugins: [
-      '@babel/plugin-transform-runtime',
-      ['@babel/plugin-proposal-decorators', { legacy: true }],
-      ['@babel/plugin-proposal-class-properties', { loose: true }],
-      'react-hot-loader/babel',
-    ],
-  };
