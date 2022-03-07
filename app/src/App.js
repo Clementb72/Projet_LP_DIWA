268991c8f9a720cfead1d6b4fd32ed7a127ae835
Clementb72 <90213043+Clementb72@users.noreply.github.com>
2022-03-07 13:42:58 +0100

    Connexion

--- app/src/App.js
+++ app/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import HomeForm from './Scenes/HomeForm';
 import Form from './Scenes/Form';
 import Navigation from './Navigation';
+import LoginForm from './Scenes/LoginForm';
 
 function App() {
   return (
     <div className="App">
-        <Form></Form>   
+        <LoginForm></LoginForm>   
     </div>
   );
 }
