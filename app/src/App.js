32783d62778fc7e10af9604d9030de1d2776fec8
unknown <sophie.catrais@gmail.com>
2022-03-16 17:48:39 +0100

    Ajout - Page Home

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import LoginForm from './Scenes/LoginForm.jsx';
-
 import '../public/style/App.scss';
 import '../public/style/style.scss';
+// import SelectGame from './pages/SelectGame.jsx';
+import Home from './pages/Home.jsx';
+
+
 
 function App() {
     return (
         <div className="App">
-            <LoginForm></LoginForm>
+            <Home/>
+            {/* <SelectGame/> */}
         </div>
     );
 }
