827b81eec057abaa0dcd61a4c4b7348d7a5686ae
Sylvain Delmas <sylvainfgtz@gmail.com>
2022-05-02 11:16:02 +0200

    style parcours

--- app/src/Scenes/SelectGame.jsx
+++ app/src/Scenes/SelectGame.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import wys_app from "../../public/Assets/images/logo-wys-app.png";
 import "../../public/style/select-game.scss";
+import illu1 from "../../public/Assets/images/illu1.png";
+import illu2 from "../../public/Assets/images/illu2.png";
+import illu3 from "../../public/Assets/images/illu3.png";
 import { Link } from 'react-router-dom';
 import Navigation from "../components/Navigation.jsx";
 
@@ -8,23 +11,25 @@ function SelectGame() {
   return (
     <div className="selectGame">
       <Navigation />
-      <div className="parcours-container">
-        <div>
-          <p>Rétroaction</p>
-          <p>Prendre du recul</p>
+      <div className="parcoursContainer">
+        <div className="parcoursFlex">
+        <div className="parcours__Card">
+          <h3>Rétroaction</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dapibus nulla in lacus blandit lacinia. Nunc pharetra, est sit amet lobortis ultricies, sem lectus convallis elit, eget varius erat leo non ex.</p>
         </div>
-        <div>
-          <p>Immersion</p>
-          <p>Analyser ses sentiments</p>
+        <div className="parcours__Card">
+          <h3>Immersion</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dapibus nulla in lacus blandit lacinia. Nunc pharetra, est sit amet lobortis ultricies, sem lectus convallis elit, eget varius erat leo non ex.</p>
         </div>
-        <div>
-          <p>Conquête</p>
-          <p>Penser au futur</p>
+        <div className="parcours__Card">
+          <h3>Conquête</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dapibus nulla in lacus blandit lacinia. Nunc pharetra, est sit amet lobortis ultricies, sem lectus convallis elit, eget varius erat leo non ex.</p>
         </div>
-      </div>
-      <Link to="/formulaire" className="button-valider">
-          C'est parti !
+        </div>
+      <Link to="/formulaire" className="buttonValider">
+          Commencer l'aventure !
       </Link>
+      </div>
     </div>
   );
 
--- app/src/components/Navigation.jsx
+++ app/src/components/Navigation.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import wys_app from "../../public/Assets/images/logo-wys-app.png";
 
 function Navigation(){
+    const [displayNav, setDisplayNav] = useState(false);
     return(
         <div className="navBar">
             <img src={wys_app} alt="image_home" className="WYSLogo"></img>
             <hr className="menuSlice"></hr>
-            <div className="toggleMenu"></div>
+            <div className="toggleMenu" onClick={() => setDisplayNav(!displayNav)}></div>
+            {displayNav ? <div className="nav">Menu</div> : null}
         </div>
     )
 }
