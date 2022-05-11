0a65936cd58d1ab26feeeddf61e71b5115252825
Nicolas <nicolas@pando-studio.com>
2022-05-11 22:58:58 +0200

    feat/interfaceFormulaire

--- app/src/components/StepBar.jsx
+++ app/src/components/StepBar.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Steps } from 'antd';
 import wystar from '../../public/Assets/images/Game_Star.svg';
+import terre from '../../public/Assets/images/terre.svg';
 
 const { Step } = Steps;
 
 function StepBar({current}) {
     return (
         <Steps current={current}>
+            <Step title="" icon={<img style={{width: "50px"}} src={terre} alt="terre" />}/>
             <Step title="" />
             <Step title="" />
             <Step title="" />
